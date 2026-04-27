2bcb985cca7469b788514fb83377b306a61a25e7
David Valenzuela <102706969+Charqican@users.noreply.github.com>
2026-04-26 20:10:30 -0400

    add nvim-fugitive for git visualizations

---
+++ lua/custom/plugins/nvim-fugitive.lua
@@ -0,0 +1,11 @@
+return {
+  'tpope/vim-fugitive',
+  cmd = { 'Git', 'G' },
+  keys = {
+    { '<leader>gs', '<cmd>Git<CR>', desc = 'Git status' },
+    { '<leader>gc', '<cmd>Git commit<CR>', desc = 'Git commit' },
+    { '<leader>gp', '<cmd>Git push<CR>', desc = 'Git push' },
+    { '<leader>gl', '<cmd>Git pull<CR>', desc = 'Git pull' },
+    { '<leader>gb', '<cmd>Git blame<CR>', desc = 'Git blame' },
+  },
+}
